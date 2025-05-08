7d8c016a8c04db73e1ddb6adc0fa38cb3b0c3313
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-08 15:18:37 +0200

    Initial commit: Setup express project

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("node:path");
+const indexRouter = require("./routes/indexRoute");
+
+const app = express();
+
+//Handle static assets
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+//Handle ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//App middleware
+app.use(express.urlencoded({ extended: true }));
+
+//Router
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
