2b8ea12f76019e21c8f80d195ad3fa9f9678843d
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-09 11:27:37 +0200

    Remove development console logs

--- src/controllers/inventoryController.js
+++ src/controllers/inventoryController.js
@@ -3,13 +3,11 @@ require("dotenv").config();
 
 async function getAllItems(req, res) {
     const items = await db.getAllItems();
-    console.log(items);
     res.render("index", {title: "All items", items: items})
 }
 
 async function getItemsInCategory(req, res) {
     const items = await db.getItemsInCategory(req.params.category);
-    console.log(items);
     res.render("index", {title: "Filtered items", items: items})
 }
 
